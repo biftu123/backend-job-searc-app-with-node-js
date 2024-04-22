799766b376b536ed951992f0e89a993b447034be
unknown <biftuge@gmail.com>
2024-04-22 21:34:13 +0300

    third

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -7,33 +7,39 @@ exports.updateUser = async (req, res) => {
   }
   try {
     const updatedUser = await user.findByIdAndUpdate(
-      req.user.id,
+      req.user.userId,
       { $set: req.body },
       { new: true }
     );
-   // const { Password, __v, ...others } = updatedUser._doc;
-    res.status(200).json('UPDATE SUCESSFUL');
+   const { Password, __v, ...others } = updatedUser._doc;
+    res.status(200).json(others);
   } catch (error) {
     // Handle error if necessary
   }
 };
 exports. deleteUser = async(req,res)=>{
     try {
-        await user.findByIdAndDelete(req.user._id);
+        await user.findByIdAndDelete(req.params._id);
         res.status(200).json('delete successfully');
         
     } catch (error) {
         
     }
 };
-exports. getuser = async(req,res)=>{
-    try {
-    const userget  =    await user.findById(req.user.id);
+exports.getuser = async (req, res) => {
+  try {
+   
+    const userget = await user.findById(req.user.userId);
+    console.log(userget);
+    if (!userget) {
+      return res.status(404).json({ error: 'User not found' });
+    }
     const { Password, __v, createdAt, ...others } = userget._doc;
     res.status(200).json(others);
-    } catch (error) {
-        
-    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
 exports.getalluser  =async(req,res)=>{
     try {
--- route/userroute.js
+++ route/userroute.js
@@ -1,10 +1,10 @@
 const route =require("express").Router();
 const usercontroller =require('../controller/usercontroller');
 const {verifyToken, verifyAndAuthorized,verifyisAdmin} = require('../midlware/verfiyToken');
-route.put('/users/',verifyAndAuthorized,usercontroller.updateUser );
+route.put('/users/update/',verifyAndAuthorized,usercontroller.updateUser );
 
 route.delete('/users/:_id',verifyAndAuthorized,usercontroller.deleteUser );
 
-route.get('/users/',verifyAndAuthorized,usercontroller.getuser );
+route.get('/users/getuser',verifyAndAuthorized,usercontroller.getuser );
 route.get('/users',verifyAndAuthorized,usercontroller.getalluser );
 module.exports = route;
